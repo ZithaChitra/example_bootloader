510fb70a419709197b1eff4e4d7f21711b1882c3
zithachitra <69644637+ZithaChitra@users.noreply.github.com>
2022-08-10 13:13:05 +0200

    update

--- bootloader.asm
+++ bootloader.asm
@@ -1,9 +1,18 @@
+; what you'll find in asm is that what 
+; things look like when you write asm code is 
+; not always how the computer works underneath - Nick Blundell 
+
+mov bx, 0x7c0
+mov ds, bx
+
+
 mov ah, 0x0e ; BIOS scrolling teletype function 
 
-mov al, 'H'
+mov al, [my_character]
 int 0x10
 
-db 'X'
+my_character:
+    db "H"
 
 jmp $ ; endless jump
 
