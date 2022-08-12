40eec086b74ae61916d506f20831b61d4a6e64c4
blessing <blessing@pop-os.localdomain>
2022-08-12 19:01:55 +0200

    find bios string

--- bootloader.asm
+++ bootloader.asm
@@ -6,49 +6,18 @@
 [org 0x7c00]
 
 ; Setup the stack away from our code.
-mov bp, 0x8000
+mov bp, 0xffff
 mov sp, bp
 
-mov dx, 0xe234
-call print_hex
+mov si, MY_MESSAGE
+call print_string
 
 jmp $ ; endless jump
 
 %include "my_functions.asm"
 
-print_hex:
-    mov si, HEX_TEMPLATE
-
-    mov bx, dx      ; bx -> 0xe234
-    shr bx, 12      ; bx -> 0x00 0e
-    mov bx, [bx + HEXABET]
-    mov [HEX_TEMPLATE + 2], bl 
-
-    mov bx, dx      ; bx -> 0xe234
-    shr bx, 8       ; bx -> 0x00 e2
-    and bx, 0x000f  ; bx -> 0x00 02
-    mov bx, [bx + HEXABET]
-    mov [HEX_TEMPLATE + 3], bl 
-
-    mov bx, dx      ; bx -> 0xe234
-    shr bx, 4       ; bx -> 0x0e 34
-    and bx, 0x000f  ; bx -> 0x00 02
-    mov bx, [bx + HEXABET]
-    mov [HEX_TEMPLATE + 4], bl 
-
-    mov bx, dx      ; bx -> 0xe234
-    and bx, 0x000f  ; bx -> 0x00 02
-    mov bx, [bx + HEXABET]
-    mov [HEX_TEMPLATE + 5], bl 
-
-    call print_string
-1    ret
-
-HEX_TEMPLATE:
-    db '0x???? ', 0
-
-HEXABET:
-    db '0123456789abcdef'
+MY_MESSAGE:
+    db "Am i loaded in memory ? ", 0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
--- my_functions.asm
+++ my_functions.asm
@@ -22,3 +22,42 @@ print_char:
     mov ah, 0x0e ; BIOS scrolling teletype function 
     int 0x10
     ret
+
+print_hex:
+    push bx
+    push si
+    mov si, HEX_TEMPLATE
+
+    mov bx, dx      ; bx -> 0xe234
+    shr bx, 12      ; bx -> 0x00 0e
+    mov bx, [bx + HEXABET]
+    mov [HEX_TEMPLATE + 2], bl 
+
+    mov bx, dx      ; bx -> 0xe234
+    shr bx, 8       ; bx -> 0x00 e2
+    and bx, 0x000f  ; bx -> 0x00 02
+    mov bx, [bx + HEXABET]
+    mov [HEX_TEMPLATE + 3], bl 
+
+    mov bx, dx      ; bx -> 0xe234
+    shr bx, 4       ; bx -> 0x0e 34
+    and bx, 0x000f  ; bx -> 0x00 02
+    mov bx, [bx + HEXABET]
+    mov [HEX_TEMPLATE + 4], bl 
+
+    mov bx, dx      ; bx -> 0xe234
+    and bx, 0x000f  ; bx -> 0x00 02
+    mov bx, [bx + HEXABET]
+    mov [HEX_TEMPLATE + 5], bl 
+
+    call print_string
+    pop si
+    pop bx
+    ret
+
+
+HEX_TEMPLATE:
+    db '0x???? ', 0
+
+HEXABET:
+    db '0123456789abcdef'
